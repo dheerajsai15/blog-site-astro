b0aab4bb7f5a82f46f8cb1b5cb963c14332cee2a
Dheeraj Sai <dheerajsaivnr@gmail.com>
2026-06-27 23:34:10 +0530

    Updated the site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://blog.dheprojects.",
+  site: "https://blog.dheprojects.in",
   integrations: [mdx(), sitemap(), tailwind()],
 });
